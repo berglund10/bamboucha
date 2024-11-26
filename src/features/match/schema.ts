8422a53915b9797301b2fb1456cfabad8decc771
Anton Berglund <antonberglund_92@hotmail.com>
2024-11-26 15:32:21 +0100

    schema for bets

---
+++ src/features/match/schema.ts
@@ -0,0 +1,15 @@
+import {
+    boolean,
+    integer,
+    pgTable,
+  } from "drizzle-orm/pg-core";
+import { usersTable } from "../team-selection/schema";
+
+export const betsTable = pgTable("bets", {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    user_id: integer().notNull().references(() => usersTable.id),
+    round_id: integer().notNull(),
+    amount: integer().notNull(),
+    accepted_by_user_id: integer().references(() => usersTable.id),
+    is_accepted: boolean().default(false).notNull(),
+  });
