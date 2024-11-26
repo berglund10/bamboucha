1c8a004a75571324c7240e7e419c30431f73bfeb
Anton Berglund <antonberglund_92@hotmail.com>
2024-11-26 16:03:35 +0100

    updated createbet page

--- src/features/match/ui/create-bet.tsx
+++ src/features/match/ui/create-bet.tsx
@@ -1,20 +1,22 @@
+import { placeBetAction } from "../actions";
+
 export function CreateBet() {
     //Skapa ett bet här, med sin unika id och round id: 1.
     //service för att skapa ett bet.
     //Uppdatera en state i parent/available-bets? Kanske revalidate(path/)
-    
-    const antonsbet = {
-        players: "Mina spelare",
-        myBet: "100kr",
-        betAccepted: false,
-    }
-    return(
+
+    return (
         <div>
-            <ul>
-                <li><button>{antonsbet.myBet}</button></li>
-                <li><button>{antonsbet.myBet}</button></li>
-                <li><button>{antonsbet.myBet}</button></li>
-            </ul>
+            <form action={placeBetAction}>
+            <label className="form-control w-full max-w-xs">
+                <div className="label">
+                    <span className="label-text">How much do you want to bet?</span>
+                </div>
+                <input type="text" name="amount" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                <div className="label">
+                </div>
+            </label>
+            </form>
         </div>
     )
 }
