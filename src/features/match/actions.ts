e0de4591c47ef67eb44fa1120eee0f5c6dfdf464
Anton Berglund <antonberglund_92@hotmail.com>
2024-11-26 16:03:17 +0100

    add match features

---
+++ src/features/match/actions.ts
@@ -0,0 +1,13 @@
+"use server"
+
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { betsService } from "./instance";
+
+export const placeBetAction = async (formData: FormData) => {
+    const amount = formData.get("amount") as string;
+    await betsService.createBet(1,1, Number(amount))
+
+  revalidatePath("/match");
+  redirect("/match");
+};
---
+++ src/features/match/service.ts
@@ -0,0 +1,18 @@
+import { Db } from "@/db/instance";
+import { betsTable } from "./schema";
+
+export const createService = (db:Db) => {
+  return {
+    createBet: async (user_id: number, round_id: number, amount: number) => {
+        const newBet = {
+            user_id,
+            round_id,
+            amount
+        }
+
+        const bet = await db.insert(betsTable).values(newBet).returning();
+
+        return bet[0];
+      },
+  };
+};
